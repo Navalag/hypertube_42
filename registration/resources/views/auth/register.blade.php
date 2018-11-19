026733e891c468343d9cc919497b7362a7a67076
Andrew <agalavan@student.unit.ua>
2018-11-19 15:23:42 +0200

    set up registration

--- registration/resources/views/auth/register.blade.php
+++ registration/resources/views/auth/register.blade.php
@@ -15,41 +15,30 @@
 				<div class="col-md-6">
 					<div class="form-group form-group-default">
 						<label>First Name</label>
-						<input type="text" name="fname" placeholder="John" class="form-control" required>
+						<input type="text" name="first_name" placeholder="John" class="form-control" value="{{ old('first_name') }}" required>
 					</div>
-					@if ($errors->has('name'))
-						<label id="fname-error" class="error" for="fname">{{ $errors->first('name') }}</label>
+					@if ($errors->has('first_name'))
+						<label id="fname-error" class="error" for="fname">{{ $errors->first('first_name') }}</label>
 					@endif
 				</div>
 				<div class="col-md-6">
 					<div class="form-group form-group-default">
 						<label>Last Names</label>
-						<input type="text" name="lname" placeholder="Smith" class="form-control" required>
+						<input type="text" name="last_name" placeholder="Smith" class="form-control" value="{{ old('last_name') }}" required>
 					</div>
-					@if ($errors->has('name'))
-						<label id="fname-error" class="error" for="fname">{{ $errors->first('name') }}</label>
+					@if ($errors->has('last_name'))
+						<label id="fname-error" class="error" for="fname">{{ $errors->first('last_name') }}</label>
 					@endif
 				</div>
 			</div>
 			<div class="row">
 				<div class="col-md-12">
 					<div class="form-group form-group-default">
-						<label>Pages User name</label>
-						<input type="text" name="uname" placeholder="yourname.pages.com (this can be changed later)" class="form-control" required>
+						<label>HyperTube User name</label>
+						<input type="text" name="username" placeholder="your.username (this can be changed later)" class="form-control" value="{{ old('username') }}" required>
 					</div>
-					@if ($errors->has('name'))
-						<label id="fname-error" class="error" for="fname">{{ $errors->first('name') }}</label>
-					@endif
-				</div>
-			</div>
-			<div class="row">
-				<div class="col-md-12">
-					<div class="form-group form-group-default">
-						<label>Password</label>
-						<input type="password" name="pass" placeholder="Minimum of 4 Charactors" class="form-control" required>
-					</div>
-					@if ($errors->has('name'))
-						<label id="fname-error" class="error" for="fname">{{ $errors->first('name') }}</label>
+					@if ($errors->has('username'))
+						<label id="fname-error" class="error" for="fname">{{ $errors->first('username') }}</label>
 					@endif
 				</div>
 			</div>
@@ -57,10 +46,32 @@
 				<div class="col-md-12">
 					<div class="form-group form-group-default">
 						<label>Email</label>
-						<input type="email" name="email" placeholder="We will send loging details to you" class="form-control" required>
+						<input type="email" name="email" placeholder="We will send loging details to you" class="form-control" value="{{ old('email') }}" required>
 					</div>
-					@if ($errors->has('name'))
-						<label id="fname-error" class="error" for="fname">{{ $errors->first('name') }}</label>
+					@if ($errors->has('email'))
+						<label id="fname-error" class="error" for="fname">{{ $errors->first('email') }}</label>
+					@endif
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-12">
+					<div class="form-group form-group-default">
+						<label>Password</label>
+						<input type="password" name="password" placeholder="Minimum of 6 Charactors" class="form-control" required>
+					</div>
+					@if ($errors->has('password'))
+						<label id="fname-error" class="error" for="fname">{{ $errors->first('password') }}</label>
+					@endif
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-12">
+					<div class="form-group form-group-default">
+						<label>Confirm Password</label>
+						<input type="password" name="password_confirmation" placeholder="Confirm password above" class="form-control" required>
+					</div>
+					@if ($errors->has('password'))
+						<label id="fname-error" class="error" for="fname">{{ $errors->first('password') }}</label>
 					@endif
 				</div>
 			</div>
