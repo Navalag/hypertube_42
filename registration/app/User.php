1707f001aca9792e9a836b3dc3109b4263af8c5b
Andrew <agalavan@student.unit.ua>
2018-11-20 18:01:46 +0200

    additional set up reset password and send mail

--- registration/app/User.php
+++ registration/app/User.php
@@ -5,6 +5,7 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use App\Notifications\ForgotPassword as ResetPasswordNotification;
 
 class User extends Authenticatable
 {
@@ -27,4 +28,10 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function sendPasswordResetNotification($token)
+    {
+        // Your your own implementation.
+        $this->notify(new ResetPasswordNotification($token));
+    }
 }
--- registration/resources/views/auth/passwords/email.blade.php
+++ registration/resources/views/auth/passwords/email.blade.php
@@ -71,4 +71,11 @@ window.onload = function()
 	</div>
 	<!-- END Login Right Container-->
 </div>
+
+<script>
+$(function()
+{
+	$('#form-login').validate()
+})
+</script>
 @endsection
