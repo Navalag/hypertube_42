dd7df1e55fd05b8bed131434efaa302cfce593bd
vsyveniu <somefellavs@gmail.com>
2019-01-08 19:29:01 +0200

    added icons

--- src/resources/views/layouts/menu.blade.php
+++ src/resources/views/layouts/menu.blade.php
@@ -21,214 +21,213 @@
 					<span class="title_aside" data="Action">{{ __('Action') }}</span>
 					<!--<button class="add_button" data="Action">+</button>-->
 				</a>
-				<span class="icon-thumbnail"><i class="pg-mail"></i></span>
+				<span class="icon-thumbnail"><i class="fab fa-accessible-icon"></i></span>
 			</li>
 			<li class="movie_genre">
 				<a href="" class="genre_direct_link">
 					<span class="title_aside" data="Adventure">{{ __('Adventure') }}</span>
 				</a>
-				<span class="icon-thumbnail"><i class="pg-social"></i></span>
+				<span class="icon-thumbnail"><i class="fas fa-dharmachakra"></i></span>
 			</li>
 			<li class="movie_genre">
 				<a href="" class="genre_direct_link">
 					<span class="title_aside" data="Animation">{{ __('Animation') }}</span>
 				</a>
-				<span class="icon-thumbnail"><i class="pg-calender"></i></span>
+				<span class="icon-thumbnail"><i class="fas fa-drafting-compass"></i></span>
 			</li>
 			<li class="movie_genre">
 				<a href="" class="genre_direct_link">
 					<span class="title_aside" data="Comedy">{{ __('Comedy') }}</span>
 				</a>
-				<span class="icon-thumbnail"><i class="pg-layouts"></i></span>
+				<span class="icon-thumbnail"><i class="far fa-grin"></i></span>
 			</li>
 			<li class="movie_genre">
 				<a href="" class="genre_direct_link">
 					<span class="title_aside" data="Crime">{{ __('Crime') }}</span>
 				</a>
-				<span class="icon-thumbnail"><i class="pg-layouts2"></i></span>
+				<span class="icon-thumbnail"><i class="fas fa-people-carry"></i></span>
 			</li>
 			<li class="movie_genre">
 				<a href="" class="genre_direct_link">
 					<span class="title_aside" data="Documentary">{{ __('Documentary') }}</span>
 				</a>
-				<span class="icon-thumbnail">Ui</span>
+				<span class="icon-thumbnail"><i class="fab fa-react"></i></span>
 			</li>
 			<li class="movie_genre">
 				<a href="" class="genre_direct_link">
 					<span class="title_aside" data="Drama">{{ __('Drama') }}</span>
 				</a>
-				<span class="icon-thumbnail"><i class="pg-form"></i></span>
+				<span class="icon-thumbnail"><i class="far fa-sad-cry"></i></span>
 
 			</li>
 			<li class="movie_genre">
 				<a href="" class="genre_direct_link">
 					<span class="title_aside" data="Family">{{ __('Family') }}</span>
 				</a>
-				<span class="icon-thumbnail"><i class="pg-grid"></i></span>
+				<span class="icon-thumbnail"><i class="fas fa-user-friends"></i></span>
 			</li>
 			<li class="movie_genre">
 				<a href="" class="genre_direct_link">
 					<span class="title_aside" data="Fantasy">{{ __('Fantasy') }}</span>
 				</a>
-				<span class="icon-thumbnail"><i class="pg-grid"></i></span>
+				<span class="icon-thumbnail"><i class="fas fa-dragon"></i></span>
 			</li>
 			<li class="movie_genre">
 				<a href="" class="genre_direct_link">
 					<span class="title_aside" data="History">{{ __('History') }}</span>
 				</a>
-				<span class="icon-thumbnail"><i class="pg-grid"></i></span>
+				<span class="icon-thumbnail"><i class="fas fa-atlas"></i></span>
 			</li>
 			<li class="movie_genre">
 				<a href="" class="genre_direct_link">
 					<span class="title_aside" data="Horror">{{ __('Horror') }}</span>
 				</a>
-				<span class="icon-thumbnail"><i class="pg-grid"></i></span>
+				<span class="icon-thumbnail"><i class="fas fa-skull-crossbones"></i></span>
 			</li>
 			<li class="movie_genre">
 				<a href="" class="genre_direct_link">
 					<span class="title_aside" data="Music">{{ __('Music') }}</span>
 				</a>
-				<span class="icon-thumbnail"><i class="pg-grid"></i></span>
+				<span class="icon-thumbnail"><i class="fas fa-volume-up"></i></span>
 			</li>
 			<li class="movie_genre">
 				<a href="" class="genre_direct_link">
 					<span class="title_aside" data="Mystery">{{ __('Mystery') }}</span>
 				</a>
-				<span class="icon-thumbnail"><i class="pg-grid"></i></span>
+				<span class="icon-thumbnail"><i class="fas fa-crow"></i></span>
 			</li>
 			<li class="movie_genre">
 				<a href="" class="genre_direct_link">
 					<span class="title_aside" data="Romance">{{ __('Romance') }}</span>
 				</a>
-				<span class="icon-thumbnail"><i class="pg-grid"></i></span>
+				<span class="icon-thumbnail"><i class="far fa-kiss-wink-heart"></i></span>
 			</li>
 			<li class="movie_genre">
 				<a href="" class="genre_direct_link">
 					<span class="title_aside" data="Science Fiction">{{ __('Science Fiction') }}</span>
 				</a>
-				<span class="icon-thumbnail"><i class="pg-grid"></i></span>
+				<span class="icon-thumbnail"><i class="fas fa-space-shuttle"></i></span>
 			</li>
 			<li class="movie_genre">
 				<a href="" class="genre_direct_link">
 					<span class="title_aside" data="TV Movie">{{ __('TV Movie') }}</span>
 				</a>
-				<span class="icon-thumbnail"><i class="pg-grid"></i></span>
+				<span class="icon-thumbnail"><i class="fas fa-tv"></i></span>
 			</li>
 			<li class="movie_genre">
 				<a href="" class="genre_direct_link">
 					<span class="title_aside" data="Thriller">{{ __('Thriller') }}</span>
 				</a>
-				<span class="icon-thumbnail"><i class="pg-grid"></i></span>
+				<span class="icon-thumbnail"><i class="fas fa-user-secret"></i></span>
 			</li>
 			<li class="movie_genre">
 				<a href="" class="genre_direct_link">
 					<span class="title_aside" data="War">{{ __('War') }}</span>
 				</a>
-				<span class="icon-thumbnail"><i class="pg-grid"></i></span>
+				<span class="icon-thumbnail"><i class="fas fa-bomb"></i></span>
 			</li>
 			<li class="movie_genre">
 				<a href="" class="genre_direct_link">
 					<span class="title_aside" data="Western">{{ __('Western') }}</span>
 				</a>
-				<span class="icon-thumbnail"><i class="pg-grid"></i></span>
+				<span class="icon-thumbnail"><i class="fab fa-sticker-mule"></i></span>
 			</li>
 			<li class="tv_genre">
 				<a href="" class="genre_direct_link" >
 					<span class="title_aside" data="Action & Adventure">{{ __('Action & Adventure') }}</span>
-					<!--<button class="add_button" data="Action">+</button>-->
 				</a>
-				<span class="icon-thumbnail"><i class="pg-mail"></i></span>
+				<span class="icon-thumbnail"><i class="fab fa-accessible-icon"></i></span>
 			</li>
 			<li class="tv_genre">
 				<a href="" class="genre_direct_link">
 					<span class="title_aside" data="Animation">{{ __('Animation') }}</span>
 				</a>
-				<span class="icon-thumbnail"><i class="pg-social"></i></span>
+				<span class="icon-thumbnail"><i class="fas fa-drafting-compass"></i></span>
 			</li>
 			<li class="tv_genre">
 				<a href="" class="genre_direct_link">
 					<span class="title_aside" data="Comedy">{{ __('Comedy') }}</span>
 				</a>
-				<span class="icon-thumbnail"><i class="pg-calender"></i></span>
+				<span class="icon-thumbnail"><i class="far fa-grin"></i></span>
 			</li>
 			<li class="tv_genre">
 				<a href="" class="genre_direct_link">
 					<span class="title_aside" data="Crime">{{ __('Crime') }}</span>
 				</a>
-				<span class="icon-thumbnail"><i class="pg-layouts"></i></span>
+				<span class="icon-thumbnail"><i class="fas fa-people-carry"></i></span>
 			</li>
 			<li class="tv_genre">
 				<a href="" class="genre_direct_link">
 					<span class="title_aside" data="Documentary">{{ __('Documentary') }}</span>
 				</a>
-				<span class="icon-thumbnail"><i class="pg-layouts2"></i></span>
+				<span class="icon-thumbnail"><i class="fab fa-react"></i></span>
 			</li>
 			<li class="tv_genre">
 				<a href="" class="genre_direct_link">
 					<span class="title_aside" data="Drama">{{ __('Drama') }}</span>
 				</a>
-				<span class="icon-thumbnail">Ui</span>
+				<span class="icon-thumbnail"><i class="far fa-sad-cry"></i></span>
 			</li>
 			<li class="tv_genre">
 				<a href="" class="genre_direct_link">
 					<span class="title_aside" data="Family">{{ __('Family') }}</span>
 				</a>
-				<span class="icon-thumbnail"><i class="pg-form"></i></span>
+				<span class="icon-thumbnail"><i class="fas fa-user-friends"></i></span>
 
 			</li>
 			<li class="tv_genre">
 				<a href="" class="genre_direct_link">
 					<span class="title_aside" data="Kids">{{ __('Kids') }}</span>
 				</a>
-				<span class="icon-thumbnail"><i class="pg-grid"></i></span>
+				<span class="icon-thumbnail"><i class="fas fa-cannabis"></i></span>
 			</li>
 			<li class="tv_genre">
 				<a href="" class="genre_direct_link">
 					<span class="title_aside" data="Mystery">{{ __('Mystery') }}</span>
 				</a>
-				<span class="icon-thumbnail"><i class="pg-grid"></i></span>
+				<span class="icon-thumbnail"><i class="fas fa-crow"></i></span>
 			</li>
 			<li class="tv_genre">
 				<a href="" class="genre_direct_link">
 					<span class="title_aside" data="News">{{ __('News') }}</span>
 				</a>
-				<span class="icon-thumbnail"><i class="pg-grid"></i></span>
+				<span class="icon-thumbnail"><i class="fas fa-broadcast-tower"></i></span>
 			</li>
 			<li class="tv_genre">
 				<a href="" class="genre_direct_link">
 					<span class="title_aside" data="Reality">{{ __('Reality') }}</span>
 				</a>
-				<span class="icon-thumbnail"><i class="pg-grid"></i></span>
+				<span class="icon-thumbnail"><i class="fas fa-diagnoses"></i></span>
 			</li>
 			<li class="tv_genre">
 				<a href="" class="genre_direct_link">
 					<span class="title_aside" data="Sci-Fi & Fantasy">{{ __('Sci-Fi & Fantasy') }}</span>
 				</a>
-				<span class="icon-thumbnail"><i class="pg-grid"></i></span>
+				<span class="icon-thumbnail"><i class="fas fa-dragon"></i></span>
 			</li>
 			<li class="tv_genre">
 				<a href="" class="genre_direct_link">
 					<span class="title_aside" data="Soap">{{ __('Soap') }}</span>
 				</a>
-				<span class="icon-thumbnail"><i class="pg-grid"></i></span>
+				<span class="icon-thumbnail"><i class="far fa-grimace"></i></span>
 			</li>
 			<li class="tv_genre">
 				<a href="" class="genre_direct_link">
 					<span class="title_aside" data="Talk">{{ __('Talk') }}</span>
 				</a>
-				<span class="icon-thumbnail"><i class="pg-grid"></i></span>
+				<span class="icon-thumbnail"><i class="fas fa-bullhorn"></i></span>
 			</li>
 			<li class="tv_genre">
 				<a href="" class="genre_direct_link">
 					<span class="title_aside" data="War & Politics">{{ __('War & Politics') }}</span>
 				</a>
-				<span class="icon-thumbnail"><i class="pg-grid"></i></span>
+				<span class="icon-thumbnail"><i class="fas fa-bomb"></i></span>
 			</li>
 			<li class="tv_genre">
 				<a href="" class="genre_direct_link">
 					<span class="title_aside" data="Western">{{ __('Western') }}</span>
 				</a>
-				<span class="icon-thumbnail"><i class="pg-grid"></i></span>
+				<span class="icon-thumbnail"><i class="fab fa-sticker-mule"></i></span>
 			</li>
 
 		</ul>
